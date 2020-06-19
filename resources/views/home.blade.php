a1b26ed9bf90be9182dc11d5b4730a3e76905242
soufianeGit2020 <sosoakabo@gmail.com>
2020-06-19 18:17:24 +0100

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,75 @@
+
+
+
+
+<!-- @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection -->
+
+<div class="card mb-3">
+    <img src="https://www.guide-metiers.ma/wp-content/uploads/2019/02/est-sidi-bennour.png" width="60" height="20" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">List of students</h5>
+        <p class="card-text">You can find here all the informatins about students in the system</p>
+
+        <table class="table">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">CNE</th>
+                <th scope="col">First name</th>
+                <th scope="col">Second Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Speciality</th>
+                <th scope="col">Operations</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($students as $student)
+                <tr>
+                    <td>{{ $student->cne }}</td>
+                    <td>{{ $student->firstName }}</td>
+                    <td>{{ $student->secondName }}</td>
+                    <td>{{ $student->age }}</td>
+                    <td>{{ $student->speciality }}</td>
+                    <td>
+
+                        <a href="{{ url('/edit/'.$student->id) }}" class="btn btn-sm btn-warning">Edit</a>
+
+                    </td>
+
+
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+
+
+
+
+
+
